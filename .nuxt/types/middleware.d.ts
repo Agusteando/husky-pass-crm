3de9a4d3f8ff6f0d267a03df3812aed3e7443a12
Agusteando <desarrollo.tecnologico@casitaiedis.edu.mx>
2026-06-29 09:39:44 -0600

    Remove

--- .nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = "access-history-admin" | "admin" | "daycare-family" | "daycare-index" | "dev-only" | "family-index" | "family" | "guest" | "home" | "personas-autorizadas" | "superadmin"
-declare module 'nuxt/app' {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
